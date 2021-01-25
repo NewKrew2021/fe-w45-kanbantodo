d89a6c811de54dde3d9794d22c7a12f4cd81458e
Geonho Kim <gunhoflash@gmail.com>
2021-01-25 18:10:32 +0900

    Add note

---
+++ src/js/Note.js
@@ -0,0 +1,22 @@
+class Note {
+  #id
+  #title
+  #subtasks
+
+  constructor({ id, title }) {
+    this.#id = id
+    this.#title = title
+  }
+
+  toHtmlString() {
+    return `
+      <div id="${this.#id}" class="note">
+        <strong>${this.#title}</strong>
+      </div>
+    `
+  }
+
+  render(wrapper) {
+    wrapper.innerHTML = this.toHtmlString()
+  }
+}
