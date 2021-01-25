7e7745405820d32355ecaa658bf1bf9a3f08436d
Geonho Kim <gunhoflash@gmail.com>
2021-01-25 18:10:59 +0900

    Add task

---
+++ src/js/Task.js
@@ -0,0 +1,22 @@
+class Task {
+  #id
+  #title
+  #subtasks
+
+  constructor({ id, title }) {
+    this.#id = id
+    this.#title = title
+  }
+
+  toHtmlString() {
+    return `
+      <div id="${this.#id}" class="task">
+        ${this.#title}
+      </div>
+    `
+  }
+
+  render(wrapper) {
+    wrapper.innerHTML = this.toHtmlString()
+  }
+}
