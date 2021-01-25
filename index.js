b467a05c7a1fb144391fc8d3c4f50843ba6cba8d
puba.yu <puba.yu@kakaocorp.com>
2021-01-25 19:07:33 +0900

    feat : mongoose 설치 및 atlas에 있는 Mongo DB와 연결
    - mongoose 패키지 설치
    - altas에 만든 Mongo DB 클러스터와 연결

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ require("dotenv").config();
 
 const Koa = require("koa");
 const serve = require("koa-static");
+const mongoose = require("mongoose");
 
 const PORT = 8000;
 const port = process.env.PORT || PORT;
 
 const app = new Koa();
+
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    dbName: "kanban-todo",
+  })
+  .then((res) => console.log(`mongoDB connected`, res))
+  .catch((err) => console.error(err));
 
 app.use(serve(__dirname + "/public"));
 
