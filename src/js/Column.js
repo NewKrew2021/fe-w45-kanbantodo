bd105bd8854233d6c8dca9c704ff1f4d299dc50d
Geonho Kim <gunhoflash@gmail.com>
2021-01-25 18:08:02 +0900

    Add column

---
+++ src/js/Column.js
@@ -0,0 +1,23 @@
+class Column {
+  #id
+  #title
+  #notes
+
+  constructor({ id, title }) {
+    this.#id = id
+    this.#title = title
+  }
+
+  toHtmlString() {
+    return `
+      <div id="${this.#id}" class="column">
+        <strong>${this.#title}</strong>
+        <p>empty</p>
+      </div>
+    `
+  }
+
+  render(wrapper) {
+    wrapper.innerHTML = this.toHtmlString()
+  }
+}
