6a744e55a09942cb3ab07fc3a422839052610180
ChanhuiSeok <chanhuicom@gmail.com>
2021-01-26 03:22:03 +0900

    Define custom api 유틸성 커스텀 api를 정의하였습니다.

--- src/common.js
+++ src/common.js
@@ -1,3 +1,42 @@
 /*
     common.js
 */
+
+export function addHTML(node, text) {
+    node.innerHTML += text;
+}
+export function html(node, text) {
+    node.innerHTML = text;
+}
+
+function dfs_q(node, target) {
+    let returnVal;
+    /* dfs 탐색 */
+    for (let element of node.children) {
+        if (element.matches(target))
+            return element;
+        if (element.hasChildNodes()) {
+            let result = dfs_q(element);
+            if (result !== undefined)  /* 찾았을 경우 */
+                returnVal = result;
+        }
+    }
+    return returnVal;
+}
+
+function dfs_q_all(nodeList, node, target){
+    for (let element of node.children) {
+        if (element.matches(target))
+            nodeList.push(element);
+        if (element.hasChildNodes())
+            dfs_q_all(nodeList, element);
+    }
+    return nodeList;
+}
+
+export function query(target) {
+    return dfs_q(document.body, target)
+}
+export function queryAll(target) {
+    return dfs_q_all([], document.body, target)
+}
