06f84c6f9f7118ffb61567438dc7aa34e0ad792d
puba.yu <puba.yu@kakaocorp.com>
2021-01-25 22:35:14 +0900

    feat: 구독한 view에게 모델의 변화를 알려주는 notify 메소드 생성
    - 구독한 view에게 모델의 변화를 알려주는 notify 메소드 생성
    - 바뀐 cardList를 적용시킨다

--- public/js/controllers/todo.controller.js
+++ public/js/controllers/todo.controller.js
@@ -2,7 +2,19 @@ class TodoController {
   constructor() {
     this.todoList = [];
   }
+
   subscribe(todo) {
     this.todoList = [...this.todoList, todo];
+  }
+
+  notify(status, changedCardList) {
+    let changedTodo = this.todoList.filter((todo) => status === todo.status);
+
+    if (changedTodo) {
+      changedTodo.forEach((todo) => (todo.cardList = changedCardList));
+      return;
+    }
+
+    this.todoList = [...this.todoList, { status: status, cardList: changedCardList }];
   }
 }
