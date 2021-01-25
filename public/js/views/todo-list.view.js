786c0d15ff65e776396ac67071980c9ce4635d38
puba.yu <puba.yu@kakaocorp.com>
2021-01-25 21:57:01 +0900

    feat : todo header Template 생성
    - todo header에 해당하는 template 생성

--- public/js/views/todo-list.view.js
+++ public/js/views/todo-list.view.js
@@ -1,4 +1,13 @@
 const TODO_TPL = {
+  todoHeader(totalCount, status) {
+    return `
+      <div class="todo-header">
+        <div class="todo-header--total-count">${totalCount}</div>
+        <div class="todo-header--title">${status}</div>
+        <div class="todo-header--add-button">+</div>
+        <div class="todo-header--delete-button">-</div>
+      </div>`;
+  },
   todoCard(content, writer) {
     return `
       <div class="todo-card">
