1764c870035224e4cb2e9876c9626d9d67d22bd4
ChanhuiSeok <chanhuicom@gmail.com>
2021-01-27 17:02:55 +0900

    Set db server using lowdb, express 확장성을 위해 express, lowdb를 활용한 서버를 만들고 post 요청이 잘 되는지 테스트를 하였습니다.

--- server/newserver.js
+++ server/newserver.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const cors = require('cors')
-const port = 3000;
+const port = 5000;
+
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));  
+app.use(express.json())
 
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('db.json');
 const db = low(adapter);
+
+
+app.get('/posts', (req, res) =>{
+    res.send('GET request');
+})
+
+// Add posts(add listview item)
+app.post('/add', (req, res)=>{
+    const data = req.body.data;
+    console.log(data);
+    db.get('posts')
+      .push(data)
+      .write();
+    res.send('Post request to the homepage');
+})
+
+app.listen(port, () => {
+    console.log(`DB Server is running on ${port}`);
+});
