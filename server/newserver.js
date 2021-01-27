13a555702b6ebd30af304feef3327060edfd962f
ChanhuiSeok <chanhuicom@gmail.com>
2021-01-28 01:01:11 +0900

    Fix bug - add/delete listview listview 아이템 하나를 지울 때 인덱스가 잘못 설정되는 버그를 수정하였습니다.

--- server/newserver.js
+++ server/newserver.js
@@ -16,7 +16,6 @@ const db = low(adapter);
 // listview item 1개 추가
 app.post('/add', (req, res) => {
     const data = req.body.data;
-    console.log(data);
     db.get('posts')
         .push(data)
         .write();
@@ -47,7 +46,6 @@ app.post('/addcard', (req, res)=>{
 // input : {cardId:_, listId:_, title:_} 형태의 data 받고 추가
 app.post('/addlist', (req, res) => {
     const { cardId, listId, title } = req.body.input;
-    console.log(req.body.input);
     if(db.get('posts').find({ id: cardId })){
         db.get('posts')
           .find({ id: cardId })
@@ -79,15 +77,15 @@ app.post('/addtask', (req, res) => {
 // [-] 특정 카드 -> 리스트뷰 아이템을 삭제할 때
 app.delete('/list/remove/:cardId/:id', (req, res) => {
     const { cardId, id } = req.params;
-    console.log(cardId, id);
+    const i_cardId = parseInt(cardId);
+    const i_id = parseInt(id);
     db.get('posts')
-      .find({ id: cardId })
+      .find({ id: i_cardId })
       .get('data')
-      .remove({ id: id })
-      .write();
+      .remove({ id: i_id })
+      .write()
     res.send('list removed successfully!');
 })
-
 
 app.listen(port, () => {
     console.log(`DB Server is running on ${port}`);
