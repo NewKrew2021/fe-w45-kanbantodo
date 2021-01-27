dc075df45fc751a35b03ea08c46134ab4df67126
puba.yu <puba.yu@kakaocorp.com>
2021-01-27 23:11:05 +0900

    feat : View의 초기화 부분을 Controller에 넣음
    - View의 초기화 부분을 controller가 관리

--- app.js
+++ app.js
@@ -6,43 +6,11 @@ import { TodoController } from "./public/js/controllers/todo.controller";
 import { TodoModel } from "./public/js/models/todo.model";
 import { TodoView } from "./public/js/views/todo.view";
 
-const TODO = "할 일";
-const DONE = "마친 일";
-
-const addButtonEvent = (model, view, controller, status) => {
-  view.getNewTodoData(async (cardData) => {
-    await model.addCard(cardData);
-    controller.notify(model.todoCardList, status);
-  });
-
-  view.deleteTodo(async (cardData) => {
-    await model.deleteTodo(cardData);
-    controller.notify(model.todoCardList, status);
-  });
-};
-
 document.addEventListener("DOMContentLoaded", async (event) => {
-  const todoModel = new TodoModel();
-  const todoView = new TodoView(TODO).init();
-  const doneView = new TodoView(DONE).init();
-
   const todoController = new TodoController();
 
-  let todoCardList = await todoModel.initData();
+  const todoModel = new TodoModel();
+  const todoCardList = await todoModel.initData();
 
-  todoController.subscribe({
-    render: todoView.render,
-    status: todoView.status,
-  });
-
-  todoController.subscribe({
-    render: doneView.render,
-    status: doneView.status,
-  });
-
-  todoController.notify(todoCardList, TODO);
-  todoController.notify(todoCardList, DONE);
-
-  todoController.addButtonEvent(todoModel, todoView, TODO);
-  todoController.addButtonEvent(todoModel, doneView, DONE);
+  todoController.init(todoCardList, TodoView, todoModel);
 });
--- public/js/controllers/todo.controller.js
+++ public/js/controllers/todo.controller.js
@@ -1,6 +1,7 @@
 class TodoController {
   constructor() {
     this.todoList = [];
+    this.todoViewList = {};
   }
 
   subscribe(todo) {
@@ -25,6 +26,22 @@ class TodoController {
       await model.deleteTodo(cardData);
       this.notify(model.todoCardList, status);
     });
+  }
+
+  init(todoCardList, TodoView, todoModel) {
+    for (let status in todoCardList) {
+      this.todoViewList[status] = new TodoView(status).init();
+
+      this.subscribe({
+        render: this.todoViewList[status].render,
+        status: this.todoViewList[status].status,
+      });
+
+      this.notify(todoCardList, status);
+      this.addButtonEvent(todoModel, this.todoViewList[status], status);
+    }
+
+    return this;
   }
 }
 
