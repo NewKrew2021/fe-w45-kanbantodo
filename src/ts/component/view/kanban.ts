8d8a33b69e4203d7a00803db2490dfc3b006b2f9
Geonho Kim <gunhoflash@gmail.com>
2021-01-29 17:45:20 +0900

    Edit kanban view

--- src/ts/component/view/kanban.ts
+++ src/ts/component/view/kanban.ts
@@ -28,7 +28,7 @@ export default class KanbanView extends View {
         <div class="children-wrapper" data-wrapper-type="column">
           <!-- columns here -->
         </div>
-        <button class="column gray" data-click-action="addColumn">Add column</button>
+        <button class="column gray ml-3 flex-center" data-click-action="addColumn">Add column</button>
       </div>
     `
   }
