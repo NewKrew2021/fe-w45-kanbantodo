cc735a1e8d15f25235b11dd4a3ce57971a6d7d48
puba.yu <puba.yu@kakaocorp.com>
2021-02-01 21:42:42 +0900

    feat : 메뉴를 닫는 이벤트 추가
    - 메뉴를 닫는 이벤트 추가

--- public/js/views/pop-up-menu.view.tsx
+++ public/js/views/pop-up-menu.view.tsx
@@ -3,15 +3,21 @@ import { $ } from "../common/utils";
 class PopUpMenuView {
   element: HTMLElement;
   menuButton: HTMLElement;
+  closeButton: HTMLElement;
 
   constructor() {
     this.element = $(".menu-pop-up");
     this.menuButton = $(".header__menu-button");
+    this.closeButton = $(".menu-pop-up__close-button");
   }
 
   init() {
     this.menuButton.addEventListener("click", () => {
       this.element.style.transform = "translateX(0)";
+    });
+
+    this.closeButton.addEventListener("click", () => {
+      this.element.style.transform = null;
     });
     return this;
   }
