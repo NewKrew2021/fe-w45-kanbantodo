9d9d182522f61d6b22c746ae4756e1e60829742b
ChanhuiSeok <chanhuicom@gmail.com>
2021-02-01 20:35:46 +0900

    delete unused code

--- server/newserver.js
+++ server/newserver.js
@@ -9,7 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
-const shortid = require('shortid');
 const adapter = new FileSync('db.json');
 const db = low(adapter);
 
