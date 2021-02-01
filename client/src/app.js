c4a07406298174882b3aee64891bbd8217c361d5
ChanhuiSeok <chanhuicom@gmail.com>
2021-02-01 13:24:11 +0900

    delete unused files

--- client/src/app.js
+++ client/src/app.js
@@ -6,13 +6,10 @@ import ListView from '../views/ListView.js';
 import TodoModel from '../models/TodoModel.js';
 import * as _dom from './util.js';
 import InputView from '../views/InputView.js';
-import ListModalView from '../views/listModalView.js';
-import ListModalModel from '../models/ListModalModel.js';
 import ColumnView from '../views/columnView.js';
 
 // model, Subject
 const todoModel = new TodoModel('http://localhost:5000/posts');
-const listmodalModel = new ListModalModel();
 
 //debugger;
 
@@ -22,10 +19,6 @@ listview.init();
 
 const inputview = new InputView(todoModel);
 inputview.init();
-
-//listModalModel
-const listmodalview = new ListModalView(listmodalModel);
-listmodalview.init();
 
 const columnview = new ColumnView(todoModel);
 columnview.init();
