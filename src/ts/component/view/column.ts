a785ed34488671b79b19f3c42979f0978b9a2711
Geonho Kim <gunhoflash@gmail.com>
2021-02-02 19:16:43 +0900

    Change action trigger to open modal: double-click

--- src/ts/component/view/column.ts
+++ src/ts/component/view/column.ts
@@ -20,7 +20,7 @@ export default class ColumnView extends View {
       <div id="${id}" class="column">
         <div class="d-flex flex-center mb-2">
           <span class="badge">${nNote}</span>
-          <strong class="flex-1 ml-2 mr-auto" role="button" data-click-action="showEditModal">${title}</strong>
+          <strong class="flex-1 ml-2 mr-auto" role="button" data-dblclick-action="showEditModal">${title}</strong>
           <button data-click-action="toggleForm">+</button>
           <button data-click-action="removeSelf">×</button>
         </div>
