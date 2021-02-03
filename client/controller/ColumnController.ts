73fb70ee8c604009e22d89d0361e5fb4df09538b
nina.oh <nina.oh@kakaocorp.com>
2021-02-03 23:41:13 +0900

    Refactor controller: separate functions

--- client/controller/ColumnController.ts
+++ client/controller/ColumnController.ts
@@ -15,17 +15,26 @@ class ColumnController {
 
     init() {
         this.model.subscribe((data: ColumnData) => {
-            this.view.render(data);
-            const columnEle = document.getElementById(data.id);
-            if (columnEle) {
-                const noteAddButton = columnEle.querySelector('.note-add .add-button')
-                const columnTitleEle = columnEle.querySelector('.column__header__text > .title');
-    
-                noteAddButton && noteAddButton.addEventListener('click', this.model.addNote);
-                columnTitleEle && columnTitleEle.addEventListener('dblclick', this.model.showEditColumnModal);
-                columnEle.addEventListener('click', this.model.showDeleteNoteModal)
-            }
+            this.renderView(data);
+            this.onEvents(data);
         })
+    }
+
+    renderView(data: ColumnData){
+        this.view.render(data);
+    }
+
+    onEvents({id}: ColumnData) {
+        const columnEle = document.getElementById(id);
+        if (columnEle) {
+            const noteAddButton = columnEle.querySelector('.note-add .add-button')
+            const columnTitleEle = columnEle.querySelector('.column__header__text > .title');
+
+            noteAddButton && noteAddButton.addEventListener('click', this.model.addNote);
+            columnTitleEle && columnTitleEle.addEventListener('dblclick', this.model.showEditColumnModal);
+            columnEle.addEventListener('click', this.model.showDeleteNoteModal)
+        }
+
     }
 }
 
--- client/controller/TodoController.ts
+++ client/controller/TodoController.ts
@@ -18,28 +18,46 @@ class TodoController {
     
     init() {
         this.model.subscribe(() => {
-            this.view.render();
-            const addColumnEle = document.querySelector('.add-column')
-            const filterEle = document.querySelector('.filter-cards > input');
-            filterEle!.addEventListener('input', this.model.onChangeFilter);
-            addColumnEle!.addEventListener('click', this.model.showAddColumnModal);
+            this.renderTodoView();
+            this.onTodoEvents();
         });
+        
         // 하위 모델이 상위 모델을 subscribe 한다.
         this.model.subscribe((data:ColumnData[]) => {
             data.forEach((colData) => {
-                const columnView = new ColumnView();
-                const columnModel = new ColumnModel(colData);
-                const columnController = new ColumnController(columnView, columnModel);
-                columnController.init();
-                columnModel.notify(colData);
-
-                const columnEle = document.getElementById(colData.id);
-                const columnDeleteButton = columnEle!.querySelector('.column__header__buttons > .remove-button')
-                
-                columnDeleteButton!.addEventListener('click', this.model.showDeleteColumnModal);
+                this.renderColumnView(colData);
+                this.onColumnEvents(colData);
             });
         })
         this.model.getData();
+    }
+    
+    renderTodoView() {
+        this.view.render();
+    }
+
+    onTodoEvents(){
+        const addColumnEle = document.querySelector('.add-column')
+        const filterEle = document.querySelector('.filter-cards > input');
+        filterEle!.addEventListener('input', this.model.onChangeFilter);
+        addColumnEle!.addEventListener('click', this.model.showAddColumnModal);
+    }
+
+    renderColumnView(colData: ColumnData) {
+        const columnView = new ColumnView();
+        const columnModel = new ColumnModel(colData);
+        const columnController = new ColumnController(columnView, columnModel);
+        columnController.init();
+        columnModel.notify(colData);
+    }
+
+    onColumnEvents({id}:ColumnData){
+        const columnEle = document.getElementById(id);
+        if (columnEle) {
+            const columnDeleteButton = columnEle.querySelector('.column__header__buttons > .remove-button')
+            columnDeleteButton && columnDeleteButton.addEventListener('click', this.model.showDeleteColumnModal);
+        }
+        
     }
 }
 
