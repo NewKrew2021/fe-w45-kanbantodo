9610f4b9fd54cd587afb89ed885e9e6a4056a1c4
puba.yu <puba.yu@kakaocorp.com>
2021-02-03 18:32:30 +0900

    feat : log data를 추가하는 기능을 model에 추가
    - log data 추가를 model에 추가

--- public/js/models/pop-up-menu.model.ts
+++ public/js/models/pop-up-menu.model.ts
@@ -1,3 +1,6 @@
+const TODO_API_HOST = "http://localhost:8000/api/todo";
+const LOG_TYPE = "log";
+
 class PopUpMenuModel {
   logList: object[];
 
@@ -5,8 +8,24 @@ class PopUpMenuModel {
     this.logList = [];
   }
 
-  addLog(log: object) {
-    this.logList = [...this.logList, log];
+  async addLog(logData: object) {
+    let res = await this.postLogData(logData);
+    let newLog = await res.json();
+
+    this.logList = [...this.logList, newLog];
+
+    return this.logList;
+  }
+
+  postLogData(logData: any): any {
+    return fetch(TODO_API_HOST, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(logData),
+    });
   }
 }
 
